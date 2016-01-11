25792d6ab2d4f9ceb08f29c04b2a6f784992b4fb
Emmanuel Vaïsse <evaisse@gmail.com>
2016-01-11 21:13:39 +0100

    Add default package config

--- package.js
+++ package.js
@@ -1,5 +1,5 @@
 Package.describe({
-  name: 'router',
+  name: 'evaisse:router',
   version: '0.0.1',
   // Brief, one-line summary of the package.
   summary: 'A Meteor hash location based router',
